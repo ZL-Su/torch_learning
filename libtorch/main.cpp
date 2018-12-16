2d019c622ccc513ace7322b9346c765af1a86128
Zhilong Su <su-zl@seu.edu.cn>
2018-12-16 16:14:03 +0800

    Add new test for opencv4.

--- libtorch/main.cpp
+++ libtorch/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
-#include <api\include\torch\torch.h>
+#include <opencv2\highgui.hpp>
+#include <opencv2\core.hpp>
+#include <opencv2\xfeatures2d.hpp>
 
 int main() try
 {
 	std::cout << "This is a testing for libtorch" << std::endl;
+	
+	const auto& _Image = cv::imread("E:\\Dgelo\\Pictures\\lady.bmp",cv::IMREAD_GRAYSCALE);
 
+	auto _Sift = cv::xfeatures2d::SIFT::create();
+	std::vector<cv::KeyPoint> _Feats;
+	_Sift->detect(_Image, _Feats);
 }
 catch (std::exception& e) {
 	std::cout << e.what() << std::endl;
