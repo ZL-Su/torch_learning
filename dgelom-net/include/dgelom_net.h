f99c61b0680bcfe8718e412ae8c2e4c302c8de15
Zhilong Su <su-zl@seu.edu.cn>
2021-09-03 16:37:17 +0800

    Add dgelom-net impl.

---
+++ dgelom-net/include/dgelom_net.h
@@ -0,0 +1,62 @@
+/****************************************************/
+///   <copyright> 2019 Zhilong Su </copyright>
+/****************************************************/
+#pragma once
+#include <string>
+#include "dgelom_net_traits.h"
+
+namespace dgelom {
+namespace detail {
+template<typename _Ty>
+class DGELOMNET_API _Graph_invoker {
+	using _Myt = _Graph_invoker;
+	using value_type = _Ty;
+	using pointer = std::add_pointer_t<value_type>;
+	using initlist = std::tuple<int64_t, int64_t>;
+public:
+	/**
+	 *\brief Constructor of the dnn graph invoker
+	 *\param [_Path] user specified path where the graph net is stored
+	 */
+	_Graph_invoker(const std::string& _Path = "./data");
+
+	/**
+	 *\brief Classifies an input image with size of {height = 256, width = 256}
+	 *\param [_Data] pointer to image data with RGB format
+	 *\param [_Size] the size {height, width} of the input image, which must be {256, 256} pixels
+	 */
+	std::string operator()(void* _Data, const initlist _Size) const;
+	std::string operator()(pointer _Data, const initlist _Size) const;
+
+private:
+	///<comment> Holds the path of graph data </comment>
+	std::string _Mypath;
+};
+}
+DGELOM_MAKE_SUPPORTED_TYPE(float)
+template<> struct is_float32<float> : std::true_type {};
+
+// TEMPLATE ALIAS of _Graph_invoker<_Ty>
+template<typename _Ty, DGELOM_ENABLE_IF(is_supported_v<_Ty>)>
+using graph_net_invoker = detail::_Graph_invoker<_Ty>;
+// SPECIAL ALIAS for single floating point type
+using graph_net_f32 = graph_net_invoker<float>;
+
+/** \Example:
+ * auto main() -> int {
+	const auto _Path = "./data/model";
+	const std::string _Impath = "./data/test/smock_b";
+
+	auto img_bgr = cv::imread(_Impath+"/PICA0044.jpg");
+	decltype(img_bgr) img_rgb;
+	cv::cvtColor(img_bgr, img_rgb, cv::COLOR_BGR2RGB);
+	decltype(img_rgb) _Input;
+	cv::resize(img_rgb, _Input, { 256, 256 }, 0, 0, cv::INTER_LINEAR);
+
+	dgelom::graph_net_f32 _Net(_Path);
+	const auto _Class = _Net(_Input.data, {_Input.rows, _Input.cols});
+	if(_Class != "empty")
+		std::cout << "The input target maybe a staff. \n";
+}
+ */
+}
---
+++ dgelom-net/src/_dgelom_net.cpp
@@ -0,0 +1,103 @@
+/****************************************************/
+///   <copyright> 2019 Zhilong Su </copyright>
+/****************************************************/
+#include "../include/dgelom_net.h"
+#include "lic/dgelom_license.h"
+#include <torch/script.h>
+
+#define DGELOM_TFIELD(_RET) \
+	template<typename _Ty> _RET dgelom::detail::
+#define DGELOM_MAKE_LIC(_ID1, _ID2) \
+	std::string("<DGELOM>")+_ID1+"-ZL@159059-"+_ID2+"</DGELOM>"
+#define DGELOM_LICENSE_KEY DGELOM_MAKE_LIC( \
+	"{Z1W07883}", "{Z1W07883}")
+	//"{WD-WXX1E74861PE}","{AJ727}")
+#define DGELOM_ACTIVATION(_KEY, _LEN) \
+	if (DGELOM_MAKE_LIC(_KEY[0],_KEY[_LEN-1])==DGELOM_LICENSE_KEY) \
+	{	_Mypath = _Path + "/dgelom_graph.pt"; }
+
+DGELOM_TFIELD() _Graph_invoker<_Ty>
+::_Graph_invoker(const std::string& _Path){
+	std::vector<std::string> _ID;
+	const auto _Len = _Get_HDD_ID(_ID);
+	DGELOM_ACTIVATION(_ID, _Len)
+}
+
+DGELOM_TFIELD(std::string) _Graph_invoker<_Ty>
+::operator()(const pointer _Data, const initlist _Size) const {
+	assert(_Data != nullptr);
+
+	if (_Mypath.empty()) {
+		std::vector<std::string> _ID;
+		const auto _Len = _Get_HDD_ID(_ID);
+		std::string _Code;
+		for (const auto _ : _ID) _Code += (_ + "-");
+		_Code.erase(_Code.end()-1);
+		return "This host has not been authorized! Please send this code " + 
+			_Code + " to dgelom_su@outlook.com for requesting authorization.";
+	}
+
+#ifdef _DEBUG
+	return ("The graph invoker is in silence for debug mode.");
+#else
+	const auto classes = { 
+		"empty", "smock_a", "smock_b", "smock_c" 
+	};
+	auto _Net = torch::jit::load(_Mypath);
+	assert(_Net != nullptr);
+
+	torch::Tensor _Src = torch::from_blob(_Data, 
+		{1, std::get<0>(_Size), std::get<1>(_Size), 3}, torch::kFloat);
+	_Src = _Src.permute({0, 3, 1, 2});
+	_Src = _Src.div(255);
+
+	const auto& _Res = _Net->forward({ _Src });
+	const auto& _Max = _Res.toTensor().max(1, true);
+	//const auto _Predicted = torch::max(_Res.toTensor(), 0);
+	const auto _Idx = std::get<1>(_Max).item<int64_t>();
+
+	return *(classes.begin() + _Idx);
+#endif
+}
+
+DGELOM_TFIELD(std::string) _Graph_invoker<_Ty>
+::operator()(void* _Data, const initlist _Size) const {
+	assert(_Data != nullptr);
+
+	if (_Mypath.empty()) {
+		std::vector<std::string> _ID;
+		const auto _Len = _Get_HDD_ID(_ID);
+		std::string _Code;
+		for (const auto _ : _ID) _Code += (_ + "-");
+		_Code.erase(_Code.end() - 1);
+		return "This host has not been authorized! Please send this code " + 
+			_Code + " to dgelom_su@outlook.com for requesting authorization.";
+	}
+
+#ifdef _DEBUG
+	return ("The graph invoker is in silence for debug mode.");
+#else
+	const auto classes = { "empty", "smock_a", "smock_b", "smock_c" };
+
+	auto _Net = torch::jit::load(_Mypath);
+	assert(_Net != nullptr);
+
+	torch::Tensor _Src = torch::from_blob(_Data,
+		{ 1, std::get<0>(_Size), std::get<1>(_Size), 3 }, torch::kByte);
+	_Src = _Src.permute({ 0, 3, 1, 2 });
+	_Src = _Src.toType(torch::kFloat);
+	_Src = _Src.div(255);
+
+	const auto& _Res = _Net->forward({ _Src });
+	const auto& _Max = _Res.toTensor().max(1, true);
+	auto _Predicted = torch::max(_Res.toTensor(), 0);
+	const auto _Idx = std::get<1>(_Max).item<int64_t>();
+
+	return *(classes.begin() + _Idx);
+#endif
+}
+
+/**
+ *\brief explicit instantiation
+ */
+template class dgelom::detail::_Graph_invoker<float>;
